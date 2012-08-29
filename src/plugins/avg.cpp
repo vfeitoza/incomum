e9da00fbb1361c7ce6680846cf96274c9320480d
Luciano Pinheiro <luciano@lucianopinheiro.net>
2012-08-28 23:40:09 -0300

    small changes.

--- src/plugins/avg.cpp
+++ src/plugins/avg.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 //AVG plugin - last check: 2012-02-02
 //example: http://af.avg.com/softw/80free/update/u8iavi4164u4162uy.bin
-int avg(string *domain, string *urlf, string *urlf)
+int avg(string *domain, string *urlf)
 {
 	if(regexMatch("\\.avg\\.c(om|z)/$", *domain)){
 		if(regexMatch("^http://(backup|a.|pupdate-aa)\\.avg\\.c(om|z)/softw/", *urlf)){
--- src/plugins/youtube.cpp
+++ src/plugins/youtube.cpp
@@ -11,7 +11,7 @@ int youtube(string *domain, string *urlf)
 			if(urlf->find("noflv=") != string::npos && urlf->find("ptk=") == string::npos){ //(noflv AND !ptk) => redirecting URL
 				return 1;
 			}else{
-				*urlf = "http://flv.youtube.inComum/?id="+get_var(*urlf, "id")+get_var(*urlf, "video_id")+"&quality="+get_var(*urlf, "fmt")+get_var(*url, "itag")+"&redirect_counter="+get_var(*url, "redirect_counter")+get_var(*url, "st")+"&begin="+get_var(*urlf,"begin")+"&range="+get_var(*urlf, "range");
+				*urlf = "http://flv.youtube.inComum/?id="+get_var(*urlf, "id")+get_var(*urlf, "video_id")+"&quality="+get_var(*urlf, "fmt")+get_var(*urlf, "itag")+"&redirect_counter="+get_var(*urlf, "redirect_counter")+get_var(*urlf, "st")+"&begin="+get_var(*urlf,"begin")+"&range="+get_var(*urlf, "range");
 			}
 		}
 		return 1;
